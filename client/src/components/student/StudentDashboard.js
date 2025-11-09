1b3eea8e0e575f2abdc4629cb7d482b9ec82f2f0
Giant220 <kaemanemakhetha2@gmail.com>
2025-11-09 12:41:12 +0200

    Update StudentDashboard.js

--- client/src/components/student/StudentDashboard.js
+++ client/src/components/student/StudentDashboard.js
@@ -27,7 +27,7 @@ const StudentDashboard = () => {
     <div className="student-dashboard">
       <nav className="navbar">
         <div className="logo-area">
-          <div className="logo" style={{ backgroundColor: '#ffda77' }}></div>
+          <div className="logo" style={{ backgroundColor: '#ffda77',opacity:0}}></div>
           <span className="brand">Student Portal</span>
         </div>
         <div className="nav-links">
